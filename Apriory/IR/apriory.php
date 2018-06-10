dd543acb88677a01ba5647d6669331a6f3b37321
Master <vinogradovmaxim06@gmail.com>
2018-06-10 21:20:36 +0300

    Априори для профессии

--- Apriory/IR/apriory.php
+++ Apriory/IR/apriory.php
@@ -2,31 +2,45 @@
 /**
  * Created by PhpStorm.
  * User: Master
- * Date: 02.06.2018
- * Time: 17:39
+ * Date: 10.06.2018
+ * Time: 18:29
  */
 header("Content-Type: text/html; charset=UTF-8");
 //
+set_time_limit(0);
+ini_set('memory_limit', '-1');
+ini_set('max_execution_time', 10000);
+ignore_user_abort(true);
+
 $host = '127.0.0.1'; // адрес сервера
 $database = 'db_voting'; // имя базы данных
 $user = 'root'; // имя пользователя
 $password = 'lkzlbgkjvf@2018'; // пароль
 $port = '3300';
-//
-//$link = mysqli_connect($host, $user, $password, $database, $port)
-//or die("Ошибка " . mysqli_error($link));
-//
-//mysqli_set_charset($link, "utf8");    https://t4tutorials.com/apriori-algorithm/
 
-$mas1 = array('Milk,Tea,Cake', 'Eggs,Tea,Cold Drink', 'Milk,Eggs,Tea,Cold Drink', 'Eggs,Cold Drink', 'Juice');
+$skills_pre = array();
+$skills = array();
+//var_dump($_POST['mas']);
+//echo "dfdsfdsdsfdsfdfs";
+$skill_pre = $_POST['mas'];
+//var_dump($skills_pre);
+//echo 'crbks';
+for($i=0;$i<count($_POST['mas']);$i++){
+    $skills[$i]=$_POST['mas'][$i];
+}
+//var_dump($skills);
+////echo json_encode($mas_for_apriory,JSON_UNESCAPED_UNICODE);
+$link = mysqli_connect($host, $user, $password, $database, $port)
+or die("Ошибка " . mysqli_error($link));
+//$skills = array('Знание Excel','Базовые знания SQL');
 
-//$mas2 = array('Milk', 'Tea', 'Cake', 'Cold Drink', 'Juice', 'Eggs');
-$mas2 = array('Milk', 'Tea', 'Cake');
-set_time_limit(0);
-ini_set('memory_limit', '200000M');
-//ini_set('memory_limit', '-1');
-ini_set('max_execution_time', 10000);
-ignore_user_abort(true);
+mysqli_set_charset($link, "utf8");
+
+
+$prof = 'Специалист по ИС';
+
+
+
 
 
 
@@ -34,38 +48,13 @@ ignore_user_abort(true);
 /////////////////////////// уникальные скилы для одной профессии
 
 
-$link = mysqli_connect($host, $user, $password, $database, $port)
-or die("Ошибка " . mysqli_error($link));
-
-mysqli_set_charset($link, "utf8");
 
 
-$prof = 'Системный аналитик';
 
-//$query1 = "SELECT DISTINCT `Компетенция` FROM `new` WHERE `Профессия`='$prof'";
-//
-//$query_result = mysqli_query($link,$query1);
-//
-//if (!$query_result)
-//{
-//    die('Ошибка выполнения запроса' . mysqli_error($link));
-//}
-////echo "$query_result";
-//$skills = array();
-//
-//while($rows = mysqli_fetch_array($query_result))
-//    //Обрабатывает ряд результата запроса, возвращая
-//    //ассоциативный массив, численный массив или оба
-//{
-//    $skills[] = $rows[0];
-//
-//}
-$skills = array('Нацеленность на результат','Умение организовать и работать в команде','Базовые знания SQL','Знание Excel',
-    'Аналитические способности','Внимательность к деталям' );
+//$skills = array('Нацеленность на результат','Умение организовать и работать в команде','Базовые знания SQL','Знание Excel',
+   // 'Аналитические способности','Внимательность к деталям',1,2,3,4,5,6,7 );
 
-echo "<pre>";
-print_r($skills);
-echo "<pre>";
+
 
 
 function pc_array_power_set($array) {
@@ -78,10 +67,7 @@ function pc_array_power_set($array) {
 }
 
 $power_set = pc_array_power_set($skills);
-echo "Возможные варианты";
-echo "<pre>";
-print_r($power_set);
-echo "<pre>";
+
 
 
 /////////////////////////////////////////////
@@ -89,21 +75,21 @@ echo "<pre>";
 // подсчитали кол-во голосов
 $count_skills = array();    // -=-------------  выкачали коды респондетов голосовавших за данную профессии
 
-    $query2 = "SELECT DISTINCT `Код_респондента` FROM `new` WHERE `Профессия`='$prof'";
-    $query_result2 = mysqli_query($link,$query2);
-    if (!$query_result2)
-    {
-        die('Ошибка выполнения запроса' . mysqli_error($link));
-    }
-    while($rows = mysqli_fetch_array($query_result2))
-    {
-        // echo "$skills[$i].' -- '.$rows[0]<br>";
-        $count_skills[] = $rows[0];
-    }
-
-echo "<pre>";
-print_r($count_skills);
-echo "<pre>";
+$query2 = "SELECT DISTINCT `Код_респондента` FROM `new` WHERE `Профессия`='$prof'";
+$query_result2 = mysqli_query($link,$query2);
+if (!$query_result2)
+{
+    die('Ошибка выполнения запроса' . mysqli_error($link));
+}
+while($rows = mysqli_fetch_array($query_result2))
+{
+    // echo "$skills[$i].' -- '.$rows[0]<br>";
+    $count_skills[] = $rows[0];
+}
+//echo "1";
+//echo "<pre>";
+//print_r($count_skills);
+//echo "<pre>";
 //echo" adsfsdfdsffffffffffffffffffff";
 //print_r($count_skills);
 //echo "count ".count($count_skills);
@@ -115,7 +101,7 @@ echo "<pre>";
 
 
 //
-echo "voices "."<br>";
+//echo "voices "."<br>";
 $query1 = "SELECT `Компетенция` FROM `new` WHERE `Профессия`='$prof'";
 
 $query_result = mysqli_query($link,$query1);
@@ -135,9 +121,9 @@ while($rows = mysqli_fetch_array($query_result))
 
 }
 
-echo "<pre>";
-print_r($voices);
-echo "<pre>";
+//echo "<pre>";
+//print_r($voices);
+//echo "<pre>";
 
 //
 //echo "ujkjdflsdfklkdsdddddddddddddddddddddddddddd";
@@ -154,7 +140,7 @@ for($i=0; $i < count($count_skills); $i++) {
         die('Ошибка выполнения запроса' . mysqli_error($link));
     }
 //echo "$query_result";
-$string='';
+    $string='';
     while ($rows = mysqli_fetch_array($query_result))
         //Обрабатывает ряд результата запроса, возвращая
         //ассоциативный массив, численный массив или оба
@@ -162,16 +148,16 @@ $string='';
 //        print($count_skills[$i]);
 //        print_r($rows[0]);
 //        $mas_of_every_[$count_skills[$i]] = $rows[0];
-    $string = $string.$rows[0].";";
+        $string = $string.$rows[0].";";
     }
     $mas_of_every_voice[] = $string;
 }
-echo "LAST!1111111111111111111111111111111111111111111111111";
+//echo "LAST!1111111111111111111111111111111111111111111111111";
 //echo "<pre>";
 //print_r($mas_of_every_voice);
 ////print_r($mas_of_every_);
 //echo "<pre>";
-echo json_encode($mas_of_every_voice,JSON_UNESCAPED_UNICODE);
+//echo json_encode($mas_of_every_voice,JSON_UNESCAPED_UNICODE);
 
 
 
@@ -190,55 +176,15 @@ echo json_encode($mas_of_every_voice,JSON_UNESCAPED_UNICODE);
 
 
 
-//function pc_array_power_set($array) {
-//// инициализируем пустым множеством
-//    $results = array(array());
-//    foreach ($array as $element)
-//        foreach ($results as $combination)
-//            array_push($results, array_merge(array($element), $combination));
-//    return $results;
-//}
-//
-//
-//
-//$power_set = pc_array_power_set($skills);
-//print_r($power_set);
-//
-//
-//
-//// Создает все возможные комбинации
-//$possible = array();
-//$first_elem = '';
-//for($i=0; $i < count($mas2); $i++){
-//    $first_elem = $first_elem.$mas2[$i].",";
-//    for($j=$i+1; $j < count($mas2); $j++){
-//        $variant = $first_elem.$mas2[$j];
-//        echo $variant."<br>";
-//        $possible[]=$variant;
-//    }
-//}
-//echo "<br>";
 
 
 
-
-
-
-
-
-
-//
-//$possible = $power_set;
-//echo "<pre>";
-//print_r($possible);
-//echo "<pre>";
-//
 $after_possible = array();
 
 $v = array();
 for ($i=0; $i < count($power_set);$i++){
     $v = $power_set[$i];
-    print_r($v);
+    //print_r($v);
     $st='';
     for ($j=0; $j < count($v);$j++){
         if ($j == count($v) - 1) {
@@ -246,15 +192,15 @@ for ($i=0; $i < count($power_set);$i++){
         } else {
             $st = $st.$v[$j].';';
         }
-        echo $st.'<br>';
+       // echo $st.'<br>';
     }
     $after_possible[]= $st;
 }
-echo "   after    ";
+//echo "   after    ";
 //echo"<pre>";
 //print_r($after_possible);
 //echo"<pre>";
-echo "nexxt step"."<br>";
+//echo "nexxt step"."<br>";
 
 
 
@@ -263,8 +209,8 @@ $main_mass = array();
 $alone = true;
 for($i=0; $i < count($after_possible); $i++) {
     $value = $after_possible[$i];
-    echo "Stroka iz randoma ".'<br>';
-    echo $value."<br>";
+  //  echo "Stroka iz randoma ".'<br>';
+   // echo $value."<br>";
 
 //    for($qw=0; $qw < count($value); $qw++) {
 //        $new_value = $value[$qw];
@@ -279,24 +225,24 @@ for($i=0; $i < count($after_possible); $i++) {
     $mas_of_uniq_from_value = explode(';', $value); // массив из компетенций 1 варианта для проверки
 
     for ($j = 0; $j < count($mas_of_every_voice); $j++) {
-        echo "Массив из  компетенций 1 голоса" . "<br>";
+      //  echo "Массив из  компетенций 1 голоса" . "<br>";
         $value2 = $mas_of_every_voice[$j];
         $mas_of_unig_from_value2 = explode(';', $value2); // массив из компетенций 1 голоса
-        print_r($mas_of_unig_from_value2);
-        echo "<br>";
-        echo "Массив из  компетенций 1 варианта для проверки" . "<br>";
-        print_r($mas_of_uniq_from_value);
-        echo "<br>";
+//        print_r($mas_of_unig_from_value2);
+//        echo "<br>";
+//        echo "Массив из  компетенций 1 варианта для проверки" . "<br>";
+//        print_r($mas_of_uniq_from_value);
+//        echo "<br>";
         // created the mas from voices
 
 
         for ($k = 0; $k < count($mas_of_uniq_from_value); $k++) {
 
             if ((in_array($mas_of_uniq_from_value[$k], $mas_of_unig_from_value2)) && ($k == count($mas_of_uniq_from_value) - 1)) { //count($mas_of_uniq_from_value)
-                echo "why here";
-                print($mas_of_uniq_from_value[$k] . " in ");
-                print_r($mas_of_unig_from_value2);
-                echo "<br>";
+//                echo "why here";
+//                print($mas_of_uniq_from_value[$k] . " in ");
+//                print_r($mas_of_unig_from_value2);
+//                echo "<br>";
                 //$string1 = '';
 //                    for($t=0;$t < count($value); $t++) {
 //                       // $string1 = $string1.$value[$t].',';
@@ -309,16 +255,16 @@ for($i=0; $i < count($after_possible); $i++) {
 //                    }
                 //$main_mass[] = $string1;
                 $main_mass[] = $value;
-                echo "Сейчас массив такой :";
-                print_r($main_mass);
+//                echo "Сейчас массив такой :";
+//                print_r($main_mass);
                 continue;
             }
 
             if (in_array($mas_of_uniq_from_value[$k], $mas_of_unig_from_value2)) {
-                echo "fantom" . "<br>";
-                print($mas_of_uniq_from_value[$k] . " in ");
-                print_r($mas_of_unig_from_value2);
-                echo "<br>";
+//                echo "fantom" . "<br>";
+//                print($mas_of_uniq_from_value[$k] . " in ");
+//                print_r($mas_of_unig_from_value2);
+//                echo "<br>";
 //                print($mas_of_uniq_from_value[$k]." in ".print_r($mas_of_unig_from_value2)."<br>");
                 // continue;
 
@@ -351,3 +297,25 @@ for($i=0; $i < count($main_mass); $i++) {
     }
 }
 
+//echo json_encode($main_mass, JSON_UNESCAPED_UNICODE);
+
+
+$mas_of_power = array();
+for ($i=0; $i < count($skills); $i++) {
+    $query2 = "SELECT COUNT(`Компетенция`) FROM `interval_table` WHERE `Компетенция`='$skills[$i]'";
+    $query_result2 = mysqli_query($link, $query2);
+    if (!$query_result2) {
+        die('Ошибка выполнения запроса' . mysqli_error($link));
+    }
+    while ($rows = mysqli_fetch_array($query_result2)) {
+        // echo "$skills[$i].' -- '.$rows[0]<br>";
+       // $mas_of_power[][$skills[$i]] = $rows[0];
+        $mas_of_power[$i]['Набор'] =$skills[$i];
+        $mas_of_power[$i]['Мощность'] =$rows[0];
+    }
+}
+//echo "<pre>";
+//print_r($mas_of_power);
+//echo "<pre>";
+arsort($mas_of_power);
+echo json_encode($mas_of_power, JSON_UNESCAPED_UNICODE);
--- Apriory/IR/index.js
+++ Apriory/IR/index.js
@@ -1,27 +1,52 @@
 $(document).ready(function() {
 var list;
 var ind = 1;
-function getList() {
+var count;
+
+    $('#aprior').click(function () {
+      //  $('.aprior').remove();
+       // $('body').append('<div class=".aprior"></div>');
+        count = $('#count').val();
+       // for(var i = 1; i <= count; i++) {
+            getList(count);
+           // $('body').append(list);
+           //  $('.selectpicker').selectpicker({
+           //      style: 'btn-info',
+           //  });
+       // }
+    });
+
+function getList(index) {
+
     $.ajax({
         url: 'getList.php', // provide correct url
         type: 'POST',
         dataType: 'JSON', // <-- since you are expecting JSON
         success: function (data) {
-list ='';
+
             console.log(data); // take a peek on the values (browser console)
-            list += "<select id='" + ind + "' class='selectpicker'  data-live-search='true'>";
-            ind++;
-            alert(ind);
-            for (var i = 0; i < data.length; i++) {
-                list += "<option>" + data[i] + "</option>";
+            for (var j = 1; j <= index; j++) {
+                list = '';
+                list += "<select id='" + j + "' class='selectpicker'  data-live-search='true'>";
+                ind++;
+                // alert(ind);
+                for (var i = 0; i < data.length; i++) {
+                    list += "<option>" + data[i] + "</option>";
+                }
+                list += '</select>';
+$('body').append(list + '<br><br>');
+               // $('.aprior').append(list + '<br><br>');
+                $('.selectpicker').selectpicker({
+                    style: 'btn-info',
+                });
             }
-            list += '</select>';
-
-
+        },
+        error: function(data) {
+            alert('Произошел сбой. Повторите попытку!');
+            return;
         }
-
     });
-    return list;
+   // return list;
 }
     $('.selectpicker').selectpicker({
         style: 'btn-info',
@@ -34,21 +59,47 @@ list ='';
 
 
     $('#add').click(function (){
-      //  $('body').append(list);
-        var slecteditem = $('#myselect').find("option:selected").val();
-        alert(slecteditem);
-        var slecteditem2 = $('#myselect2').find("option:selected").val();
-        alert(slecteditem2);
-        var slecteditem3 = $('#1').find("option:selected").val();
-        alert(slecteditem3);
+        var mas_for_apriori = [];
+       for(var i =1; i <= count; i++) {
+           var val = $('#' + i).find("option:selected").val();
+           alert(val);
+           mas_for_apriori[i-1] = val;
+       }
+       var table ='';
+           $.ajax({
+               url: 'apriory.php', // provide correct url
+               type: 'POST',
+               data: {mas: mas_for_apriori},
+               dataType: 'JSON'})// <-- since you are expecting JSON
+               .done(function (data) {
+                  console.log(data);
+
+               table+='<table class="table table-striped table-hover"><thead><tr><th>Набор</th><th>Мощность</th></tr>' +
+                   '</thead> <tbody>';
+                  for (var i in data) {
+
+                      table+= '<tr><td>'+data[i].Набор+'</td><td>'+data[i].Мощность+'</td></tr>';
+                  }
+                  table+='</tbody>';
+                  $('body').append(table);
+               })
+               .error( function(data) {
+                   console.log(data);
+                   alert('Произошел сбой. Повторите попытку!');
+
+                   return;
+               });
+           });
+
+    // $('#add2').click(function () {
+    //     $('.aprior').remove();
+    //     var list = getList();
+    //     $('body').append('<div class="aprior"></div>');
+    //     $('.aprior').append(list);
+    //     $('.selectpicker').selectpicker({
+    //         style: 'btn-info',
+    //     });
+    // });
 
 
-    });
-    $('#add2').click(function () {
-        var list = getList();
-        $('body').append(list);
-        $('.selectpicker').selectpicker({
-            style: 'btn-info',
-        });
-    });
 });
